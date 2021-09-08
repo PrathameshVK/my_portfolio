7b988185ec063b01abf2d512091340de510daf81
Prathamesh Kulkarni <prathameshvk50@gmail.com>
2021-09-08 10:25:34 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <div className="header">
+        <div className="logo">
+
+        </div>
+        <div className="navbar">
+          <ul>
+            <li>About Me</li>
+            <li>Projects</li>
+            <li>Contact Me</li>
+          </ul>
+        </div>
+      </div>
+      <div className="mainBody">
+        <div className="mainIntro">
+          <p>Hello,<br/>
+            I am Prathamesh
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
